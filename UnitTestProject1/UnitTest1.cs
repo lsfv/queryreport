f0aebcd480cd6b3317e8480220c1dfc20ca49310
linson <lsfv@163.com>
2020-03-04 05:32:11 +0800

    delete discard file .

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -214,7 +214,7 @@ namespace UnitTestProject1
         {
             DataTable dataTable = Common.incUnitTest.GetDatatableCustomCount(record1);
             string errmsg;
-            CUSTOMRP.BLL.TemplateManager.GenerateXlsxExcel(dataTable, out errmsg,pathtemplate, CUSTOMRP.BLL.TemplateManager.STRFIRST_SHEETNAME);
+            //CUSTOMRP.BLL.TemplateManager.GenerateXlsxExcel(dataTable, out errmsg,pathtemplate, CUSTOMRP.BLL.TemplateManager.STRFIRST_SHEETNAME);
         }
 
         //pre :0 ,1,2,1000.
@@ -224,7 +224,7 @@ namespace UnitTestProject1
         {
             DataTable dataTable = Common.incUnitTest.GetDatatableCustomCount(record2);
             string errmsg;
-            CUSTOMRP.BLL.TemplateManager.UpdataData4XlsxExcel(dataTable, CUSTOMRP.BLL.TemplateManager.STRFIRST_SHEETNAME, out errmsg, pathtemplate);
+           // CUSTOMRP.BLL.TemplateManager.UpdataData4XlsxExcel(dataTable, CUSTOMRP.BLL.TemplateManager.STRFIRST_SHEETNAME, out errmsg, pathtemplate);
         }
 
     
